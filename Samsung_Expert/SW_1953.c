c1b98de59bc20191a3023a3e2778b679888766b6
markers2 <lleeyj94@gmail.com>
2018-04-14 01:30:41 +0900

    Add all

--- Samsung_Expert/SW_1953.c
+++ Samsung_Expert/SW_1953.c
@@ -12,7 +12,7 @@ int visit[50][50] = { 0 };
 int pipe[8][4] = { 
 	{0, 0, 0, 0}, {1, 1, 1, 1}, 
 	{1, 0, 1, 0}, {0, 1, 0 ,1}, 
-	{1, 1, 0, 0}, {0 ,1, 1, 0}, 
+	{1, 1, 0, 0}, {0, 1, 1, 0}, 
 	{0, 0, 1, 1}, {1, 0, 0, 1} 
 };
 int dir[4][2] = { {-1, 0}, {0, 1}, {1, 0}, {0, -1} };
@@ -35,7 +35,6 @@ int main(void) {
 			}
 
 		func(R, C, 1);
-
 		printf("#%d %d\n", test, cnt);
 
 	} // while()
@@ -48,17 +47,17 @@ void func(int x, int y, int t) {
 	int i, j, dx, dy, p;
 	
 	if (isInside(x, y) == 0) return; // 범위 밖
-	if (t > L || visit[x][y] == 1 || map[x][y] == 0) return; // escape condition
+	if (t > L || (visit[x][y] < t && visit[x][y] > 0) || map[x][y] == 0) return; // escape condition
 
-	cnt++;
-	visit[x][y] = 1;
+	if (visit[x][y] == 0) { cnt++; visit[x][y] = t;	} 
+	else { if (visit[x][y] > t) visit[x][y] = t; }
 
 	for (i = 0; i < 4; i++) {
 		p = pipe[map[x][y]][i]; // 파이프 방향
 
 		if (p == 1) {
 			dx = x + dir[i][0]; dy = y + dir[i][1];
-			if (isInside(dx, dy) == 0) continue;
+			if (isInside(dx, dy) == 0 || (visit[dx][dy] < t + 1 && visit[dx][dy] > 0)) continue;
 			if (pipe[map[dx][dy]][(i + 2) % 4] == 1) func(dx, dy, t + 1);
 		} // if
 	} // for(i)	
