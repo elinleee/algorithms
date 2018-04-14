3fd06d7b342abd8b86e02c0a87c5ff76f076c467
markers2 <lleeyj94@gmail.com>
2018-04-14 19:16:40 +0900

    complete 2382.c

--- Samsung_Expert/SW_2382.c
+++ Samsung_Expert/SW_2382.c
@@ -3,17 +3,17 @@
 
 #include <stdio.h>
 
-int isInside(int, int);
 int isEdge(int, int);
 void func();
 
 int T, N, M, K;
 int map[100][100][2] = { 0 }; // 각 좌표에 있는 미생물의 수 & 군집의 방향
+int cell[1000][2] = { 0 }; // 군집들의 위치 좌표
 int dir[4][2] = { { -1, 0 },{ 1, 0 },{ 0, -1 },{ 0, 1 } };
 
 int main(void) {
 
-	int i, j, tx, ty, total;
+	int i, j, tx, ty, sum;
 	int t = 0;
 	int k;
 
@@ -22,30 +22,30 @@ int main(void) {
 	while (t < T) {
 
 		t++;
-		total = 0;
-		for (i = 0; i < N; i++) for (j = 0; j < N; j++) { map[i][j][0] = 0; map[i][j][1] = 0; }
+		sum = 0;
+		for (i = 0; i < N; i++) {
+			for (j = 0; j < N; j++) {
+				map[i][j][0] = 0; map[i][j][1] = 0;
+			}
+		}
 
 		scanf("%d %d %d", &N, &M, &K); // 맵 크기, 시간, 군집 수
 		for (i = 0; i < K; i++) {
 			scanf("%d %d", &tx, &ty);
 			scanf("%d %d", &map[tx][ty][0], &map[tx][ty][1]);
+			cell[i][0] = tx; cell[i][1] = ty;
 		} // for(i)
 
 		for (i = 0; i < M; i++) func();
 
-		for (i = 0; i < N; i++) for (j = 0; j < N; j++) total += map[i][j][0];
+		for (i = 0; i < N; i++) for (j = 0; j < N; j++) sum += map[i][j][0];
 
-		printf("#%d %d\n", t, total);
+		printf("#%d %d\n", t, sum);
 
 	}// while()
 
 	return 0;
 } // main()
-
-int isInside(int x, int y) {
-	if (x >= 0 && x < N && y >= 0 && y < N) return 1;
-	else return 0;
-} // isInside()
 
 int isEdge(int x, int y) {
 	if (x == 0 || x == N - 1 || y == 0 || y == N - 1) return 1;
@@ -54,53 +54,54 @@ int isEdge(int x, int y) {
 
 void func() {
 
-	int i, j, k;
-	int x, y, c, d, sum, max, maxdir;
-	int dmap[100][100][2] = { 0 };
+	int i, j;
+	int x, y, c, d, sum, cnt = 0;
+	int dx, dy, dc, dd, dmap[100][100][2] = { 0 }, max[100][100][2] = { 0 };
+
+	for (i = 0; i < K; i++) {
+
+		x = cell[i][0]; y = cell[i][1];
+		c = map[x][y][0]; d = map[x][y][1];
+
+		if (x == 0 && y == 0) continue;
+
+		dx = x + dir[d - 1][0]; dy = y + dir[d - 1][1];
+		dc = c; dd = d;
+
+		// 가장자리
+		if (isEdge(dx, dy) == 1) {
+			dc /= 2;
+			if (d == 1) dd = 2;
+			else if (d == 2) dd = 1;
+			else if (d == 3) dd = 4;
+			else if (d == 4) dd = 3;
+		} // if()
+
+		  // 합쳐질 경우
+		if (dmap[dx][dy][0] != 0) {
+			if (c > max[dx][dy][0]) {
+				max[dx][dy][0] = c;
+				max[dx][dy][1] = d;
+			} // if()
+			dc += dmap[dx][dy][0];
+			dd = max[dx][dy][1];
+			cell[i][0] = 0; cell[i][1] = 0;
+		} // if()
+		else {
+			max[dx][dy][0] = c;
+			max[dx][dy][1] = d;
+			cell[i][0] = dx; cell[i][1] = dy;
+		}
+
+		dmap[dx][dy][0] = dc; dmap[dx][dy][1] = dd;
+
+	} // for(i)
 
 	for (i = 0; i < N; i++) {
 		for (j = 0; j < N; j++) {
-
-			sum = 0; max = 0; maxdir = 0;
-
-			for (k = 0; k < 4; k++) {
-
-				x = i + dir[k][0]; y = j + dir[k][1];
-				if (isInside(x, y) == 0) continue;
-				c = map[x][y][0]; d = map[x][y][1];
-
-				if (c == 0 && d == 0) continue;
-
-				if ((k == 0 && d == 2) || (k == 1 && d == 1) ||
-					(k == 2 && d == 4) || (k == 3 && d == 3)) {
-
-					sum += map[x][y][0];
-
-					if (map[x][y][0] > max) {
-						max = map[x][y][0];
-						maxdir = map[x][y][1];
-					} // if()
-				} // if()
-			} // for(k)
-
-			if (isEdge(i, j) == 1) { // 가장자리
-				dmap[i][j][0] = sum / 2;
-				if (maxdir == 1) dmap[i][j][1] = 2;
-				else if (maxdir == 2) dmap[i][j][1] = 1;
-				else if (maxdir == 3) dmap[i][j][1] = 4;
-				else if (maxdir == 4) dmap[i][j][1] = 3;
-			}
-			else {
-				dmap[i][j][0] = sum;
-				dmap[i][j][1] = maxdir;
-			} // if ..  else
-
-		} // for(j)
-	} // for(i)
-
-	for (i = 0; i < N; i++) for (j = 0; j < N; j++) {
-		map[i][j][0] = dmap[i][j][0]; map[i][j][1] = dmap[i][j][1];
-		dmap[i][j][0] = 0; dmap[i][j][1] = 0;
+			map[i][j][0] = dmap[i][j][0];
+			map[i][j][1] = dmap[i][j][1];
+		}
 	}
 
 } // func()
